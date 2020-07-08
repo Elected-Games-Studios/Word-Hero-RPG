b02ab3cacf9fd09ccda903e1eaea03475c960c96
Kyle Cribben <kylecribben@gmail.com>
2020-07-08 15:07:36 -0400

    Fuck you

---
+++ _V2/CharectorStats.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class CharectorStats
+{
+    private static List<List<int>> HeroList;
+    private static List<int> heroDefault = new List<int> { 0, 0, 0, 0, 0 };
+    public static int SkillPoints;
+    public static string SaveManagerData()
+    {
+        string sendSave = "";
+        sendSave += SkillPoints.ToString();
+        sendSave += "|";
+        for (int x = 0; x < HeroList.Count; x++)
+        {
+            for (int y = 0; y < HeroList[x].Count; y++)
+            {
+                sendSave += HeroList[x][y].ToString();
+                sendSave += ",";
+            }
+            sendSave += "|";
+        }
+        return sendSave;
+    }
+    public static void LoadManagerData(string input)
+    {
+        if (input == "")
+        {
+            NewGame();
+            SkillPoints = 0;
+        }
+        else
+        {
+            SkillPoints = Convert.ToInt32(input.Substring(0, (input.IndexOf(',') - 1)));
+            input.Remove(0, 1);
+
+            int counter = 0;
+            int counting = 0;
+            while (input.IndexOf('|') > 0)
+            {
+                HeroList.Add(heroDefault);
+                while (input.IndexOf(',') < input.IndexOf('|'))
+                {
+                    HeroList[counter][counting] = Convert.ToInt32(input.Substring(0, (input.IndexOf(',') - 1)));
+                    input.Remove(0);
+                    counting++;
+                }
+                counting = 0;
+                input.Remove(0);
+                counter++;
+            }
+        }
+    }
+    public static void NewGame()
+    {
+        List<int> tempList = heroDefault;
+        tempList.Insert(0, 0);
+        HeroList.Add(tempList);
+    }
+    public static List<int> UnlockedCharectors()
+    {
+        List<int> Allhero = new List<int> { };
+        for (int i = 0; i < HeroList.Count; i++)
+        {
+            Allhero.Add(HeroList[i][0]);
+        }
+        return Allhero;
+    }
+    public static List<int> CharStats(int charNum) { return (HeroList[charNum]); }
+    public static void AutoSkill(int CharUsed)
+    {
+        int temp = HeroList[CharUsed][1];
+        if (HeroList[CharUsed][1] > HeroList[CharUsed][2]) HeroList[CharUsed][2]++;
+        else if (HeroList[CharUsed][1] > HeroList[CharUsed][3]) HeroList[CharUsed][3]++;
+        else if (HeroList[CharUsed][1] > HeroList[CharUsed][4]) HeroList[CharUsed][4]++;
+        else if (HeroList[CharUsed][1] > HeroList[CharUsed][5]) HeroList[CharUsed][5]++;
+        else HeroList[CharUsed][1]++;
+
+    }
+    public static void ManualSkill(int skill, int CharUsed) => HeroList[CharUsed][skill]++;
+    public static void LevelUP() => SkillPoints++;
+}
---
+++ _V2/WordData.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Management.Instrumentation;
+using System.Linq.Expressions;
+
+class WordData
+{
+    private static List<int[,]> regionsSaves; //stacked score over star repeats by region.
+    private static int[,] regionDataScore = new int[3, 25]
+    { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
+    private static int[,] regionDataStars = new int[3, 25]
+    { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
+    private static int numberOfRegions = 3;
+    private static List<int> DifMods = new List<int> { 30, 70, 140 };
+    public static string SaveManagerData()
+    {
+        string sendSave = "";
+        for (int x = 0; x < regionsSaves.Count; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                for (int z = 0; z < 25; z++)
+                {
+                    sendSave += regionsSaves[x][y, z].ToString();
+                    sendSave += ",";
+                }
+            }
+            sendSave += "|";
+        }
+        return sendSave;
+    }
+    public static void MakeNewRegion()
+    {
+        regionsSaves.Add(regionDataScore);
+        regionsSaves.Add(regionDataStars);
+    }
+    private static void MakeAllRegions()
+    {
+        int number = (Region.AllRegions.Count / 3);
+        for (int x = 0; x < number; x++)
+        {
+            MakeNewRegion();
+        }
+    }
+    public static void LoadManagerData(string input)
+    {
+        if (input == "")
+        {
+            MakeAllRegions();
+        }
+        else
+        {
+            MakeAllRegions();
+            int counter = 0;
+            while (input.IndexOf('|') > 0)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    for (int y = 0; y < 25; y++)
+                    {
+                        regionsSaves[counter][x, y] = Convert.ToInt32(input.Substring(0, (input.IndexOf(',') - 1)));
+                        input.Remove(0);
+                    }
+                }
+                input.Remove(0);
+                counter++;
+            }
+        }
+    }
+    public static string GetDataLevel(int region, int level, int Dificulty /*all data starts at 0 for these ints*/)
+    {
+        string levelData;
+        int location = region * 3 + Dificulty;
+        levelData = Region.AllRegions[location][level];
+        return levelData;
+    }
+    public static string GetWords(int book, int word)
+    {
+        switch (book)
+        {
+            case 5:
+                return (WordMaster.fiveLetter[word]);
+            case 6:
+                return (WordMaster.sixLetter[word]);
+            case 7:
+                return (WordMaster.sevenLetter[word]);
+            default:
+                return (WordMaster.eightLetter[word]);
+        }
+    }
+    public static void DataToSave(int score, int starCount, int region, int level, int dificulty)
+    {
+        int tempReg = region * 2;
+        regionsSaves[tempReg][dificulty, level] = score;
+        tempReg += 1;
+        regionsSaves[tempReg][dificulty, level] = starCount;
+    }
+}
