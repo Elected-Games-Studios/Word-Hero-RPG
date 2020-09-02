97116285d53cb6ab234f1e13f86b35d842fff658
Kyle Cribben <kylecribben@gmail.com>
2020-09-01 21:16:17 -0400

    Here Is the most Up to Date

---
+++ _V2/GameWordHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class GameWordHandler
+{
+    private static int Region, Level, Dificulty;
+    private static List<int> WordsIndex;
+    private static List<string> Words;
+    public static void StartLevel()
+    {
+        WordsIndex = WordDatav2.GetDataLevel();
+        GetActualWords();
+    } 
+    public static void SetLevelData(int region, int level, int dificulty)
+    {
+        Region = region;
+        Level = level;
+        Dificulty = dificulty;
+    }
+    private static void GetActualWords()
+    {
+        int tempcount = WordsIndex.Count();
+        tempcount = tempcount / 2;
+        for (int x = 0; x < tempcount; x++)
+        {
+            Words = WordDatav2.GetWords(WordsIndex[0], WordsIndex[1+x*2]);
+        }
+    }
+}
