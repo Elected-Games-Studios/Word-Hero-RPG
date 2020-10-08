b6f325994cdbdcf387d5677820a2c350da9145fe
Dustin Kline <klinetek@gmail.com>
2020-10-08 15:24:19 -0400

    Unity Proto Code
    Fleshing out the static functionality of the Unity side.

---
+++ _V2/Unity Code/SoloCombat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+public class SoloCombat : MonoBehaviour
+    {
+        // grab player stats, get dmg, health, timer mods
+        
+        // Get Word
+        
+        // grab Timer, start
+        
+        // read live input from word circle, Dependency inject word circle
+        
+        // dmg Enemy
+        
+        // Get New Enemy
+        
+        // Get new Word
+        
+        // play again, back to main menu
+    }
