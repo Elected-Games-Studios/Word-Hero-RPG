e4c11f33bcceda451713902fa502ee5d13397597
Dustin Kline <klinetek@gmail.com>
2020-11-19 01:12:43 -0500

    Touching youuuuu touching meeeeee
    suite caroleine

--- _V2/GameWordHandler.cs
+++ _V2/GameWordHandler.cs
@@ -11,45 +11,81 @@ public static class GameWordHandler
     private static int Region, Level, Dificulty;
     private static List<int> WordsIndex;
     public static List<string> Words;
-    public static void StartLevel()
+    public static List<string> wordsUsed;
+
+    public static void StartLevel() //called on awake for 
     {
         WordsIndex = WordData.GetDataLevel(Region, Level, Dificulty);
         GetActualWords();
-    } 
-    public static void SetLevelData(int region, int level, int dificulty)
+    }
+
+    public static void
+        SetLevelData(int region, int level,
+            int dificulty) // called at beginning of level, call from Mono around the scene
     {
         Region = region;
         Level = level;
         Dificulty = dificulty;
     }
+
     private static void GetActualWords()
     {
         int tempcount = WordsIndex.Count();
         tempcount = tempcount / 2;
         for (int x = 0; x < tempcount; x++)
         {
-            Words.Add(Convert.ToString(WordData.GetWords(WordsIndex[0], WordsIndex[1+x*2])));
+            Words.Add(Convert.ToString(WordData.GetWords(WordsIndex[0], WordsIndex[1 + x * 2])));
         }
     }
+
     public static int GetPlayerAttack()
     {
         return CharecterStats.CurrentHero[0];
     }
+
     public static int GetPlayerDefence()
     {
         return CharecterStats.CurrentHero[1];
     }
+
     public static int GetPlayerSpeed()
     {
         return CharecterStats.CurrentHero[2];
     }
+
     public static int GetPlayerLuck()
     {
         return CharecterStats.CurrentHero[3];
     }
+
     public static int GetPlayerCrit()
     {
         return CharecterStats.CurrentHero[4];
     }
 
+    public static void Shuffle()
+    {
+
+        List<string> tempList = wordsUsed;
+        string tempWord = tempList[(tempList.Count - 1)];
+        tempList = new List<string>();
+        for (int x = 0; x < tempWord.Count(); x++) tempList.Add(tempWord[x].ToString());
+        int counter = wordsUsed.Count;
+        for (int x = 0; x < counter; x++)
+        {
+            int remover = UnityEngine.Random.Range(0, tempList.Count);
+            string tempLetter = tempList[remover];
+            tempList.RemoveAt(remover);
+            //your code here
+        }
+    }
+
+    public static void WordBreak(int currentword)
+    {
+        wordsUsed = new List<string> { };
+        string[] tempArrString = Words[currentword].Split(',');
+        wordsUsed = tempArrString.ToList();
+        wordsUsed.RemoveAt(0);
+    }
 }
+
---
+++ _V2/Unity Code/LetterListener.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class LetterListener : MonoBehaviour
+{
+    [SerializeField]
+    private eventManagerv2 em;
+    void OnEnable()
+    {
+        eventManagerv2.onLetterHit += AddLetterToCurrent;
+    }
+
+    void AddLetterToCurrent(int letterId) //PARAMS HAVE TO MATCH THE DELEGATE SIG
+    {
+        if(letterId == Int32.Parse(gameObject.tag))
+            Debug.Log(letterId);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        em.FireHit(int.Parse(gameObject.tag));
+    }
+
+    void OnDisable()
+    {
+        eventManagerv2.onLetterHit -= AddLetterToCurrent;
+    }
+}
