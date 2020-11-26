2146c3d616adae61caa31922444e39429bf80e40
Dustin Kline <klinetek@gmail.com>
2020-11-25 23:53:09 -0500

    Fixed some syntax issues Kline side
    just some fun syntax stuffs

--- _V2/CharectorStats.cs
+++ _V2/CharectorStats.cs
@@ -49,7 +49,7 @@ public class CharectorStats
         { {1,45,2580,750,500,50}, {150,790,6305,2000,5715,1100} };
     private static int[,] T5Stats = new int[2,6]
         { {1,100,5055,1000,750,100}, {250,2600,12525,3500,12000,3000} };
-    private static string[,] NamesAndtiers = new List<string[49,2]>
+    private static string[,] NamesAndtiers = new string[72,2]
     { {"Folk","T0"},{"Fighter","T1"},{"Ranger","T1"},{"Rogue","T1"},{"Mage","T1"},{"Cleric","T1"},{"Barbarian","T1"},{"Monk","T1"},{"Hunter","T2"},{"Soldier","T2"},{"Wizard","T2"},{"Acolyte","T2"},{"Thief","T2"},{"Gambler","T2"},{"Archer","T2"},{"Duelist","T2"},{"Berserker","T2"},{"Bard","T2"},{"Trapper","T2"},{"Blade Master","T3"},{"War Mage","T3"},{"Knight","T3"},{"Lancer","T3"},{"Druid","T3"},{"Sorcerer","T3"},{"Priest","T3"},{"Warlock","T3"},{"Arcane Trickster","T3"},{"Assassin","T3"},{"Tactician","T3"},{"Sage (Bard+)","T3"},{"Spell Sniper","T3"},{"Grand Marksman","T3"},{"Dervish","T3"},{"Hightened Monk","T3"},{"Commander","T3"},{"Magician","T3"},{"Jester","T3"},{"Falconer","T3"},{"Tinkerer","T3"},{"Hex Blade","T4"},{"Paladin","T4"},{"Weapon Master","T4"},{"Spellblade","T4"},{"Death Knight","T4"},{"Shadow","T4"},{"Juggernaut","T4"},{"Dragoon","T4"},{"Archmage","T4"},{"Eldrich Knight","T4"},{"Elementalist","T4"},{"Archdruid","T4"},{"Invoker","T4"},{"Shadowmancer","T4"},{"Avatar","T4"},{"Avenger","T4"},{"Blue Mage","T4"},{"Technomancer","T4"},{"Beast Master","T4"},{"Vampire Hunter","T4"},{"Arcane Arrow","T4"},{"Slayer/Executioner","T4"},{"Horizon Walker","T4"},{"Holy Ranger","T4"},{"Lycanthrope","T4"},{"Pirate Lord","T4"},{"Bombardier","T4"},{"Spirit Guardian","T4"},{"Aspect of Gaia","T5"},{"Dragonmaster","T5"},{"Avatar of Vengeance","T5"},{"Chronomancer","T5"} };
     public static int SkillPoints;
     public static List<int> CurrentHero;
@@ -59,7 +59,7 @@ public class CharectorStats
         int[] tempStats = new int[7] { };
         int heroNumber = HeroList[chosenCharecter][0];
         int heroLvl = HeroList[chosenCharecter][1];
-        string tier = NamesAndtiers[heroNumber][1];
+        string tier = NamesAndtiers[heroNumber][1].ToString();
 
         tempStats[0] = heroNumber;
         tempStats[1] = heroLvl;
