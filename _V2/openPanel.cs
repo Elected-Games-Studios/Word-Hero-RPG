4238948b54ef04aa801c27d8da37d7e5f00f55f7
Joe <jsharick7@gmail.com>
2020-12-31 19:20:20 -0500

    tweening region screen, transitions from main menu

---
+++ _V2/openPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openPanel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Panel;
+
+   public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            Panel.gameObject.SetActive(true);
+        }
+    }
+}
