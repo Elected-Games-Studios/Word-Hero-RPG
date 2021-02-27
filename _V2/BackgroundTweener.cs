ab6f9a285c25fd696344972f16a942cf4e7a4ef1
Joe <jsharick7@gmail.com>
2021-02-27 10:18:23 -0500

    tweenbackgrounds

--- _V2/BackgroundTweener.cs
+++ _V2/BackgroundTweener.cs
@@ -7,30 +7,36 @@ public class BackgroundTweener : MonoBehaviour
     public List<Image> backgrounds;
 
     private GameObject previous;
+
     //public void setBackground(int level)
     //{
     //    backgrounds[level].SetActive(true);
     //    previous = backgrounds[level];
     //}
-    private void Awake()
+ 
+    public void changeBackground(int level)
     {
-        previous = backgrounds[0].gameObject;
-    }
-    public IEnumerator changeBackground(int level)
-    {
-
-        fadePrevious();
+        if (level != 0) { previous = backgrounds[level - 1].gameObject; }
+        if (previous) { removePrevious(); }
         backgrounds[level].gameObject.SetActive(true);
-        LeanTween.alpha(backgrounds[level].GetComponent<RectTransform>(), 0f, 0f);
-        LeanTween.alpha(backgrounds[level].GetComponent<RectTransform>(), 1f, 1f);
-        yield return new WaitForSeconds(1f);
-        previous = backgrounds[level].gameObject;
         
+            Debug.Log("level " + level);
+            LeanTween.alpha(backgrounds[level].GetComponent<RectTransform>(), 0f, 0f);
+            LeanTween.alpha(backgrounds[level].GetComponent<RectTransform>(), .5f, 2f);//take 2 sec
+        
+       
     }
 
-    private void fadePrevious()
+    private void removePrevious()
     {
-
+        previous.SetActive(true);
+        //test line
+        LeanTween.alpha(previous.GetComponent<RectTransform>(), .5f, 0f);
+        //end test
+        LeanTween.alpha(previous.GetComponent<RectTransform>(), 0f, 2f).setOnComplete(setInactive); 
+    }
+    private void setInactive()
+    {
         previous.SetActive(false);
     }
 }
