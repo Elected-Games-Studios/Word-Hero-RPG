32387a6a117e6b3633d95afc32a40ed021d5870c
Joe <jsharick7@gmail.com>
2021-03-14 21:45:46 -0400

    Cleaned Up folder Structure

---
+++ _V2/Menu Scripts/InitializeHeroList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitializeHeroList : MonoBehaviour
+{
+    void Start()
+    {
+
+        SaveManager.LoadSplit(" # # ");
+        //Temp Debug Game Master Values
+        //GameMaster.Region = 0;
+        //GameMaster.Level = 0;
+        //GameMaster.Difficulty = 0;
+
+        Invoke("NextScene", 2f);
+    }
+
+    private void NextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
